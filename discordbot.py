c033cd7db88ab79352ea2ea21662adcb806c8ece
graphixfaculty <58206231+graphixfaculty@users.noreply.github.com>
2024-04-24 23:49:26 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import Context
 from os import getenv
-import openai
+from openai import OpenAI
 import traceback
 import asyncio
 
@@ -56,8 +56,8 @@ async def on_message(message):
             print(bot_response)
 
             # Generate voice from text
-            # client = OpenAI()
-            response = audio.speech.create(
+            client = OpenAI()
+            response = client.audio.speech.create(
                 model="tts-1",
                 voice="alloy",
                 input=bot_response
